9cf7d48a87002a8a80a178f69289305b8a043288
Reinout van Rees <reinout@vanrees.org>
2023-06-21 22:11:53 +0200

    Handling extension seperately

--- denoter/core.py
+++ denoter/core.py
@@ -2,29 +2,34 @@ import datetime
 from dataclasses import dataclass
 from pathlib import Path
 
+# from typing import Optional
+
 
 @dataclass
 class DenoteMetadata:
     title: str
     timestamp: datetime.datetime
     tags: list[str]
+    extension: str
 
 
 @dataclass
-class BasicFileMetadata:
-    filename: str
+class BasicFileInfo:
+    name: str
+    extension: str
     creation_date: datetime.datetime
 
 
-@dataclass
-class DenoteFileMetadata:
-    creation_date: datetime.datetime
-    slug: str
-    tags: list[str]
+# @dataclass
+# class DenoteFileInfo:
+#     creation_date: datetime.datetime
+#     slug: str
+#     tags: list[str]
 
 
-def basic_file_metadata(file: Path) -> BasicFileMetadata:
-    filename = file.name
+def extract_basic_file_info(file: Path) -> BasicFileInfo:
+    name = file.stem  # Stem is the filename without the extension.
+    extension = file.suffix  # Note: including the leading dot.
     creation_date = datetime.datetime.fromtimestamp(file.stat().st_ctime)
     print(creation_date)
-    return BasicFileMetadata(filename=filename, creation_date=creation_date)
+    return BasicFileInfo(name=name, extension=extension, creation_date=creation_date)
--- denoter/tests/test_core.py
+++ denoter/tests/test_core.py
@@ -3,7 +3,8 @@ from pathlib import Path
 from denoter import core
 
 
-def test_basic_file_metadata():
+def test_extract_basic_file_info():
     this_file = Path(__file__)
-    result = core.basic_file_metadata(this_file)
-    assert result.filename == "test_core.py"
+    result = core.extract_basic_file_info(this_file)
+    assert result.name == "test_core"
+    assert result.extension == ".py"
