211288b9915bc78a704f1692a4b4973ce0605d25
Reinout van Rees <reinout@vanrees.org>
2023-06-21 23:09:20 +0200

    Added basic file info extraction

--- denoter/core.py
+++ denoter/core.py
@@ -1,16 +1,21 @@
 import datetime
+import logging
 from dataclasses import dataclass
 from pathlib import Path
 
 # from typing import Optional
 
+logger = logging.getLogger(__name__)
+
 
 @dataclass
 class DenoteMetadata:
+    """This is what's needed to generate a filename"""
+
     title: str
+    extension: str
     timestamp: datetime.datetime
     tags: list[str]
-    extension: str
 
 
 @dataclass
@@ -31,5 +36,17 @@ def extract_basic_file_info(file: Path) -> BasicFileInfo:
     name = file.stem  # Stem is the filename without the extension.
     extension = file.suffix  # Note: including the leading dot.
     creation_date = datetime.datetime.fromtimestamp(file.stat().st_ctime)
-    print(creation_date)
-    return BasicFileInfo(name=name, extension=extension, creation_date=creation_date)
+    result = BasicFileInfo(name=name, extension=extension, creation_date=creation_date)
+    logger.debug("Basic file info extracted from %s: %s", file, result)
+    return result
+
+
+def metadata_from_file(file: Path) -> DenoteMetadata:
+    info = extract_basic_file_info(file)
+    # We assume for the moment we have nothing else.
+    return DenoteMetadata(
+        title=info.name,
+        extension=info.extension,
+        timestamp=info.creation_date,
+        tags=[],
+    )
--- denoter/tests/test_core.py
+++ denoter/tests/test_core.py
@@ -8,3 +8,11 @@ def test_extract_basic_file_info():
     result = core.extract_basic_file_info(this_file)
     assert result.name == "test_core"
     assert result.extension == ".py"
+
+
+def test_metadata_from_file():
+    this_file = Path(__file__)
+    result = core.metadata_from_file(this_file)
+    assert result.title == "test_core"
+    assert result.extension == ".py"
+    assert result.tags == []
