23a4db653c2af9ff0cb550327923f3e059459756
Reinout van Rees <reinout@vanrees.org>
2023-06-21 23:11:21 +0200

    Added file options to the correct argument...

--- denoter/scripts.py
+++ denoter/scripts.py
@@ -12,10 +12,10 @@ app = typer.Typer()
 
 @app.command()
 def main(
-    files: Annotated[list[Path], typer.Argument(help="Files to rename.")],
-    verbose: Annotated[
-        bool, typer.Option(help="Be verbose.", dir_okay=False, readable=True)
-    ] = False,
+    files: Annotated[
+        list[Path], typer.Argument(help="Files to rename.", dir_okay=False, exists=True)
+    ],
+    verbose: Annotated[bool, typer.Option(help="Be verbose.")] = False,
 ):
     log_level = logging.DEBUG and verbose or logging.INFO
     logging.basicConfig(level=log_level, format="%(levelname)s: %(message)s")
